53d3b6442bac7a33378ee42c8cac481fecbce033
Karli Vare <karlivare@Karlis-MacBook-Pro.local>
2024-02-01 15:37:54 -0500

    reading writing assignment

--- nodejs-http/exercise-express-routing/parameters/app.js
+++ nodejs-http/exercise-express-routing/parameters/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let bodyParser = require('body-parser');
+app.use(bodyParser.json({ extended: false }));
+
+app.post('/<path>', (req, res) => {
+  console.log('req.body', req.body) // contains incoming data
+})
+
 const products = [
   {
     id: 1,
@@ -22,7 +29,21 @@ const products = [
 
 app.get("/", (req, res) => res.send("Hello API!"));
 
-app.get("/products/:id", (req, res) => {});
+app.get("/products/:id", (req, res) => {
+  res.json(products.find(p => p.id === +req.params.id));
+});
+app.get("/products", (req, res) => {
+  const page = +req.query.page;
+  const pageSize = +req.query.pageSize;
+
+  if (page && pageSize) {
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    res.json(products.slice(start, end));
+  } else {
+    res.json(products);
+  }
+});
 
 app.get("/products", (req, res) => {});
 
